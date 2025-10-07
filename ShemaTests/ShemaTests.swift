acab0766b27c86a915cbe0fa69e86f8369543b8a
Benson Arafat <benpaul320@gmail.com>
2025-10-07 22:16:28 +0100

    Initial Commit

---
+++ ShemaTests/ShemaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShemaTests.swift
+//  ShemaTests
+//
+//  Created by Benson Arafat on 07/10/2025.
+//
+
+import Testing
+@testable import Shema
+
+struct ShemaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
